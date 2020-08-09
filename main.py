15980b15d0b95109761738e78809d58b9a1ba6b2
lululuNMZS <l334532871@hotmail.com>
2020-08-09 20:29:13 +0800

    2020008092029

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import winapi
+from PIL import Image
+
+#get the colors,just for test
+#img = Image.open("C:/Users/Administrator/Desktop/4.png")
+#this_img = img.crop((322,501,338,516))
+#print(this_img.getcolors())
 
 hwnd = winapi.find_the_hwnd()
 
